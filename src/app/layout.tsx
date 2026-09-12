3af78e9eeaeef95fc56743775b10b2a1ae026d7e
mharismazhar-369 <mharis.mazhar@gmail.com>
2026-09-12 14:04:47 +0500

    google verification meta tag addition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="google-site-verification" content="szixoqouSou5jIVNRsufe2AB6yoINqFQbIoIdU5CL6c" />
+      </head>
       <body className="antialiased">
         <ThemeProvider>
           <AuthProvider>
